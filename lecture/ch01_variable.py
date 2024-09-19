77eabcb9cfc78c0e5e5a1baee0cc0bc34184ffb4
wawajjub <nasjh5@naver.com>
2024-09-19 12:45:37 +0900

    add ch02

--- lecture/ch01_variable.py
+++ lecture/ch01_variable.py
@@ -20,14 +20,14 @@ print("Hello \nPython")
 print('Hello Python')
 print('Hello Python')
 print('Hello Python')
-# 자료형(type)
+#2. 자료형(type)
 #    - Python의 모든 자료형은 객체(Object)
 #    - 정수형(int):1, -1, 0
 #    - 실수형(float): 3.14, 0.0
 #    - 문자열(String): "Hi", 'Hi'
 #    - 논리형(boolean): True, False
 
-# 형 변환(Type Casting)
+#3. 형 변환(Type Casting)
 #    - Type Casting을 사용하여 값을 원하는 자료형으로 변환
 #    - int(): ()안의 값을 정수형으로 변환
 #    - float(): ()안의 값을 실수형으로 변환
@@ -43,16 +43,60 @@ print(int(b))
 print(int(c))
 print(float(c))
 print(float(d))
-print(int(d)) #STR(실수) -> 정수 ERROR 발생!
+#print(int(d)) #STR(실수) -> 정수 ERROR 발생!
 
-# None Type
+#4. None Type
 #    - 사용 금지!
 #    - Null == None
 #     -> NullPointerException(크리티컬 오류 -> 프로그램 종료)
 #    - 만약 None, Null이 필요하면 ""쓰기
 
-# 변수 생성 및 초기화
+#5. 변수 생성 및 초기화
 #    - num = 5
 #    - Python은 변수 선언시 type를 지정하지 않음!(동적 타이핑 언어)
 #    - =(대입연산자) -> 우측에 값을 좌측에 대입
-#    - 
+#    - 초기화: 초기 변수를 생성하면 변수를 생성한 메모리 공간에
+#             쓰레기 파일들이 존재, 변수에 값을 대입하면
+#             쓰레기 파일들이 삭제(초기화) 돼고 값만 저장!
+# 
+# 6. 명명규칙(Naming Rule)
+#    - 변수, 함수, 클래스 등의 사용자 정의 이름에 사용!
+#    - 명확하고 알아보기 쉽게!
+#    1. 영어 대소문자, 숫자, 특수문자(_, -)만 사용
+#    2. 숫자로 시작할수 없음
+#       abc123(o), 123abc(x)
+#    3. 영어 대소문자 구별
+#       Abc != abc != ABC
+#    4. 예약어 사용 불가
+#       *예약어: Python에서 미리 선점하여 사용중인 키워드
+#       print, for, while, if, else, class, and, return
+#       import, def, pass, ...
+
+# 7. 네이밍 메서드
+#    - 변수, 함수, 클레스 등의 사용자 정의 이름에 사용하는 기법
+#    - 프로그래밍 언어별로 Naming Mehtod가 다름
+#    1) PascalCase (StdunentNumber)
+#    2) camelCase (stdunentNumber)
+#    3) snake_case (stdunent_number)
+#    4) kebab-case (stdunent-number)
+
+#          변수         함수        클래스
+# JAVA     카멜         카멜        파스칼
+# PYTHON  스네이크     스네이크     파스칼
+
+# 8. 상수
+#    - 변하지 않는 수 
+#    - *상수는 반드시 선언 및 초기화(고정)를 함께
+#    - 전부다 대문자 사용
+MAX_VALUE = 30 #상수
+
+# 9. 동적 출력
+#    - print() -> 출력(+변수)
+
+student_num = 24
+name = "홍길동"
+print("조선대학교 24학번 홍길동입니다.") # 하드 코딩(지양)
+#    1. format() - old
+print("조선대학교 {}학번 {}입니다.".format(student_num, name))
+#    2. f-string - new
+print(f"조선대학교 {student_num}학번 {name}입니다.")
---
+++ lecture/ch02_string.py
@@ -0,0 +1,64 @@
+# 문자열의 이해(String)
+#    - 활용 예:데이터분석, 자연어 처리 응용,
+#              사용자로부터 값 입력받고 처리(유효성 체크) 
+
+# 1. 문자열 인덱스(index)
+#    - 문자열은 각 문자마다 순서(인덱스) 있음
+#    - 인덱스 시작은 0
+#    - 인덱스는 공백 포함
+intro = "Hello Python" # 문자열 길이(12), 인덱스(0~11), 역인덱스(-12~-1)
+
+# 2. 문자 추출
+#    - 인덱스 범위 벗어난 경우(Error: index out of range)
+print(intro[0]) # H
+print(intro[-2]) # o
+
+# 3. 문자열 슬라이싱(문자열 추출)
+#    - [시작:끝] 끝-1
+#    - [0:11] -> 0~10
+print(intro[:]) # 처음부터(0) 끝까지
+print(intro[:6]) # 처음부터(0) 5까지
+print(intro[3:]) # 3부터 끝까지(-1)
+
+date = "20240919 12:27"
+date[0:4]
+
+# 4. 문자열 함수
+#    - len(): 문자열 길이 계산
+print(len(intro)) # 12
+#    - upper() and lower(): 대소문자 변경
+print(intro.upper())
+print(intro.lower())
+
+#    - replace(): 특정 문자 치환
+print(intro.replace("H", "J"))
+
+#    - split(): 구분자를 기준으로 문자열 분할(기본값: 공백)
+print(intro.split())
+print(intro.split("o"))
+
+#    - strip(): 문자열의 좌우공백 제거
+#       ㄴ rstrip(), lstrip()
+# "             ccw             " -> "ccw" 
+print(intro.strip())
+
+#    - in(): 특정 문자열 포함하는지 확인(True or False 출력)
+print("python" in intro) # 대소문자 구별(정확히)
+
+#    - find() and rfind(): 문자열 내부 인덱스 출력
+print(intro.find("H"))
+print(intro.find("Python")) # 첫번째 글자의 인덱스
+print(intro.find("o"))      # 좌측에서 부터 먼저 찾는거
+print(intro.rfind("o"))     # 우측에서 부터 먼저 찾는거
+print(intro.rfind("hi"))    # 없는경우 -1 출력
+
+# 문제1. "cherry1004"만 출력
+#  - 조건 아이디의 길이는 상시 달라집니다.
+# 아이디: "cherry123", "cherry10", "cherry0"
+id = "cherry1004@gmail.com"
+
+# 문제2. 도메인 추출
+#  ex) WWW.naver.com
+#      WWW.google.com
+#      WWW.daum.com
+# naver, google, daum을 추출하는 코드
